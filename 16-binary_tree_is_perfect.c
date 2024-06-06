e17a2ee918d645eeaf956bdaf29ba93cb24c16fc
zoubjd <zouhairbajdouri19@gmail.com>
2024-06-06 21:07:41 +0100

    from 14 to 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
@@ -19,19 +20,16 @@ return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- * Return: Balance factor
- */
-int binary_tree_balance(const binary_tree_t *tree)
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if the tree is perfect, 0 otherwise
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 if (tree == NULL)
 {
 return (0);
 }
-
-int left_height = binary_tree_height(tree->left);
-int right_height = binary_tree_height(tree->right);
-
-return (left_height - right_height);
+return (binary_tree_height(tree->left) == binary_tree_height(tree->right));
 }
