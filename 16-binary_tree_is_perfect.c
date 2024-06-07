7ad8af899919188df9f6c5bc5f61ae7270ae3806
zoubjd <zouhairbajdouri19@gmail.com>
2024-06-07 13:24:44 +0100

    push

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,66 +1,52 @@
 #include "binary_trees.h"
 
 /**
- * find_depth - Measures the depth
- * @node: Pointer to the root node of the tree
- * Return: 1 or 0
+ * height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree
  */
 
-int find_depth(const binary_tree_t *node)
+int height(const binary_tree_t *tree)
 {
-int depth = 0;
+	size_t left, right;
 
-while (node != NULL)
-{
-depth++;
-node = node->left;
-}
-return (depth);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	left = height(tree->left);
+	right = height(tree->right);
+	if (left > right)
+	{
+		return (left + 1);
+	}
+	else
+	{
+		return (right + 1);
+	}
 }
 
 /**
- * is_perfect - finds if the tree is perfect
- * @tree: Pointer to the root node of the tree
- * @depth: Depth of the tree
- * @level: Level of the tree
- * Return: 1 or 0
- */
-
-int is_perfect(const binary_tree_t *tree, int depth, int level)
-{
-if (tree == NULL)
-{
-return (0);
-}
-
-if (tree->left == NULL && tree->right == NULL)
-{
-return (depth == level + 1);
-}
-
-if (tree->left == NULL || tree->right == NULL)
-{
-return (0);
-}
-
-return (is_perfect(tree->left, depth, level + 1) &&
-	is_perfect(tree->right, depth, level + 1));
-}
-
-/**
- * binary_tree_is_perfect - finds if the tree is perfect
- * @tree: Pointer to the root node of the tree
- * Return: 1 or 0
- */
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if the tree is perfect, 0 otherwise
+*/
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (tree == NULL)
-{
-return (0);
-}
+	int left_height, right_height;
 
-int depth = find_depth(tree);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 
-return (is_perfect(tree, depth, 0));
+	return (left_height == right_height);
 }
